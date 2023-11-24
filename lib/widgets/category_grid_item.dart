eb9a4b9e12f519bc3594c320e52731e738ef29d7
Yassine <yassine.abouzia@gmail.com>
2023-11-24 13:10:22 +0100

    added category screen with data

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+final borderRadius = BorderRadius.circular(16);
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: borderRadius,
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
